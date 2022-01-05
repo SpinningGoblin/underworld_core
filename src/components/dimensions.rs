0fd2bd5e9317dc3eb63229fda991914bb5376cb1
Derrick Plotsky <derrickplotsky@gmail.com>
2022-01-04 20:17:24 -0800

    Rework dimensions
    I'm removing units for now, and I'll just make everything the same
    units with different ways to describe them later.
    
    I'm also adding a way to get a description of the height based on a
    species, that way I can say things like "tall Goblin".

--- src/components/dimensions.rs
+++ src/components/dimensions.rs
@@ -1,12 +1,32 @@
-#[derive(Clone, Debug)]
-pub struct Dimensions {
-    pub height: i32,
-    pub width: i32,
-    pub units: DimensionUnits,
-}
+use super::species::Species;
 
 #[derive(Clone, Debug)]
-pub enum DimensionUnits {
-    METER,
-    CENTIMETER,
+pub struct Dimensions {
+    pub height: f32,
+    pub width: f32,
+}
+
+impl Dimensions {
+    pub fn describe_height_for_species(&self, species: &Species) -> String {
+        match species {
+            &Species::Goblin => {
+                if self.is_taller_than(1.2) {
+                    "tall".to_string()
+                } else if self.is_shorter_than(0.6) {
+                    "short".to_string()
+                } else {
+                    "".to_string()
+                }
+            }
+            _ => "".to_string(),
+        }
+    }
+
+    fn is_taller_than(&self, height: f32) -> bool {
+        self.height > height
+    }
+
+    fn is_shorter_than(&self, height: f32) -> bool {
+        self.height < height
+    }
 }
