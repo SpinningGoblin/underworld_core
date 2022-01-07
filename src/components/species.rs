d86e276d5f54e3f9f4e0b9a0f4dcec2edf610beb
Derrick Plotsky <derrickplotsky@gmail.com>
2022-01-06 18:16:27 -0800

    Add height ranges for species
    This will let other components get the height range for a species
    which will let them decide if the instance of the species they are
    looking at is tall or short.

--- src/components/species.rs
+++ src/components/species.rs
@@ -1,24 +1,40 @@
+use std::ops::Range;
+
 use crate::describable::Describable;
 
 #[derive(Clone, Debug)]
 pub enum Species {
-    BugBear,
-    Elf,
+    Bugbear,
     Goblin,
-    Human,
     Kobold,
     Ogre,
     Orc,
     Unknown,
 }
 
+const SMALL_HEIGHT: Range<f32> = 0.6..1.2;
+const MEDIUM_HEIGHT: Range<f32> = 1.2..2.05;
+const LARGE_HEIGHT: Range<f32> = 2.05..4.4;
+const UNKNOWN_HEIGHT: Range<f32> = 0.6..4.4;
+
+impl Species {
+    pub fn height_range(&self) -> Range<f32> {
+        match *self {
+            Self::Bugbear => MEDIUM_HEIGHT,
+            Self::Goblin => SMALL_HEIGHT,
+            Self::Kobold => SMALL_HEIGHT,
+            Self::Ogre => LARGE_HEIGHT,
+            Self::Orc => MEDIUM_HEIGHT,
+            Self::Unknown => UNKNOWN_HEIGHT,
+        }
+    }
+}
+
 impl Describable for Species {
     fn describe(&self) -> String {
         match *self {
-            Self::BugBear => "Bug Bear".to_string(),
-            Self::Elf => "Elf".to_string(),
+            Self::Bugbear => "Bugbear".to_string(),
             Self::Goblin => "Goblin".to_string(),
-            Self::Human => "Human".to_string(),
             Self::Kobold => "Kobold".to_string(),
             Self::Ogre => "Ogre".to_string(),
             Self::Orc => "Orc".to_string(),
@@ -33,12 +49,7 @@ mod tests {
 
     #[test]
     fn describe_when_bugbear() {
-        assert_eq!("Bug Bear", Species::BugBear.describe());
-    }
-
-    #[test]
-    fn describe_when_elf() {
-        assert_eq!("Elf", Species::Elf.describe());
+        assert_eq!("Bugbear", Species::Bugbear.describe());
     }
 
     #[test]
@@ -49,11 +60,6 @@ mod tests {
     #[test]
     fn describe_when_kobold() {
         assert_eq!("Kobold", Species::Kobold.describe());
-    }
-
-    #[test]
-    fn describe_when_human() {
-        assert_eq!("Human", Species::Human.describe());
     }
 
     #[test]
