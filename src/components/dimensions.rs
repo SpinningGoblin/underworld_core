6d41f7b3f65eedbe73b964d9a632e35fb9babab8
Derrick Plotsky <derrickplotsky@gmail.com>
2022-01-06 18:19:43 -0800

    Add a describe method for species
    Dimensions aren't really something that can be described by itself,
    because they are very tied to what the dimension is on. So instead I'm
    adding a helper method for specifically species (right now) and I'll be
    adding more later.

--- src/components/dimensions.rs
+++ src/components/dimensions.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 use super::species::Species;
 
 #[derive(Clone, Debug)]
@@ -6,21 +8,22 @@ pub struct Dimensions {
     pub width: f32,
 }
 
+const TALL: &str = "tall";
+const SHORT: &str = "short";
+const AVERAGE_HEIGHT: &str = "";
+
 impl Dimensions {
     pub fn describe_height_for_species(&self, species: &Species) -> String {
-        match species {
-            &Species::Goblin => self.goblin_height(),
-            _ => "".to_string(),
-        }
+        self.calculate_height(&species.height_range())
     }
 
-    fn goblin_height(&self) -> String {
-        if self.is_taller_than(1.2) {
-            "tall".to_string()
-        } else if self.is_shorter_than(0.6) {
-            "short".to_string()
+    fn calculate_height(&self, height_range: &Range<f32>) -> String {
+        if self.is_taller_than(height_range.end) {
+            TALL.to_string()
+        } else if self.is_shorter_than(height_range.start) {
+            SHORT.to_string()
         } else {
-            "".to_string()
+            AVERAGE_HEIGHT.to_string()
         }
     }
 
@@ -30,5 +33,45 @@ impl Dimensions {
 
     fn is_shorter_than(&self, height: f32) -> bool {
         self.height < height
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::components::species::Species;
+
+    use super::Dimensions;
+
+    #[test]
+    fn describe_height_for_species_when_taller() {
+        let dimensions = Dimensions {
+            height: 1.5,
+            width: 0.1,
+        };
+        assert_eq!(
+            "tall",
+            dimensions.describe_height_for_species(&Species::Goblin)
+        );
+    }
+
+    #[test]
+    fn describe_height_for_species_when_shorter() {
+        let dimensions = Dimensions {
+            height: 0.4,
+            width: 0.1,
+        };
+        assert_eq!(
+            "short",
+            dimensions.describe_height_for_species(&Species::Goblin)
+        );
+    }
+
+    #[test]
+    fn describe_height_for_species_when_average() {
+        let dimensions = Dimensions {
+            height: 0.7,
+            width: 0.1,
+        };
+        assert_eq!("", dimensions.describe_height_for_species(&Species::Goblin));
     }
 }
