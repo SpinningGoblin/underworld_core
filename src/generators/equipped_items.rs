f7b46316cdba345c720fbe1726f131c90f8def3c
Derrick Plotsky <derrickplotsky@gmail.com>
2022-01-16 22:45:19 -0800

    Fix equipped location gen
    This was occasionally broken because I had an empty range.

--- src/generators/equipped_items.rs
+++ src/generators/equipped_items.rs
@@ -14,23 +14,34 @@ pub struct EquippedItemPrototype<T: Display + Clone + Debug> {
     pub equipped_location_chance: usize,
 }
 
+impl<T: Display + Clone + Debug> EquippedItemPrototype<T> {
+    fn equipped_location(&self) -> String {
+        let mut rng = rand::thread_rng();
+
+        if self.equipped_locations.is_empty() {
+            return "".to_string();
+        }
+
+        let equipped_location_roll: usize = rng.gen_range(0..=100);
+        if equipped_location_roll > self.equipped_location_chance {
+            return "".to_string();
+        }
+
+        let index = rng.gen_range(0..self.equipped_locations.len());
+        match self.equipped_locations.get(index) {
+            Some(equipped_location) => equipped_location.clone(),
+            _ => "".to_string(),
+        }
+    }
+}
+
 impl<T: Display + Clone + Debug> Generator<EquippedItem<T>> for EquippedItemPrototype<T> {
     fn generate(&self) -> EquippedItem<T> {
         let item = self.generator.generate();
 
         let mut rng = rand::thread_rng();
         let hidden_roll: usize = rng.gen_range(0..=100);
-
-        let equipped_location_roll: usize = rng.gen_range(0..=100);
-        let equipped_location = if equipped_location_roll <= self.equipped_location_chance {
-            let index = rng.gen_range(0..self.equipped_locations.len());
-            match self.equipped_locations.get(index) {
-                Some(equipped_location) => equipped_location.clone(),
-                _ => "".to_string(),
-            }
-        } else {
-            "".to_string()
-        };
+        let equipped_location = self.equipped_location();
 
         EquippedItem {
             item,
