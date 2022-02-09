33eb6244498a1c7eba4189e15a3894dc2d1a187f
Derrick Plotsky <derrickplotsky@gmail.com>
2022-02-08 22:11:45 -0800

    Print out fixture positions

--- examples/describe_room.rs
+++ examples/describe_room.rs
@@ -4,7 +4,15 @@ pub fn main() {
     let npc_names = vec!["Brognar".to_string(), "Zug".to_string()];
     let room_prototype = RoomPrototype::build_random(npc_names, 1..4);
     let room = room_prototype.generate();
-    println!("{}", room);
+    println!("{}", &room);
     println!();
-    println!("{}", room.look_at_inhabitants());
+    println!("{}", &room.look_at_inhabitants());
+
+    println!("===============");
+
+    for fixture_position in &room.fixture_positions {
+        println!("{}", &fixture_position);
+        println!();
+        println!("===============");
+    }
 }
