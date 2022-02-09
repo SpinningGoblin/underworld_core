cff0fac306b0b9c593e20b43f8c2b0a3751436e3
Derrick Plotsky <derrickplotsky@gmail.com>
2022-02-08 21:48:42 -0800

    Add group_descriptor
    This will be a way to describe a grouping of either fixtures or npcs.

--- src/components/rooms.rs
+++ src/components/rooms.rs
@@ -2,5 +2,6 @@ pub mod descriptor;
 pub mod descriptor_position;
 pub mod dimensions;
 pub mod fixture_position_descriptor;
+pub mod group_descriptor;
 pub mod room;
 pub mod room_type;
---
+++ src/components/rooms/group_descriptor.rs
@@ -0,0 +1,33 @@
+use std::fmt::Display;
+
+#[cfg(feature = "bevy_components")]
+use bevy_ecs::prelude::Component;
+#[cfg(feature = "serialization")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "bevy_components", derive(Component))]
+#[cfg_attr(feature = "serialization", derive(Deserialize, Serialize))]
+pub enum GroupDescriptor {
+    A,
+    AFew,
+    AGangOf,
+    AGroupOf,
+    ALone,
+    ASingle,
+    Some,
+}
+
+impl Display for GroupDescriptor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            GroupDescriptor::AFew => write!(f, "a few"),
+            GroupDescriptor::AGangOf => write!(f, "a gang of"),
+            GroupDescriptor::AGroupOf => write!(f, "a group of"),
+            GroupDescriptor::ALone => write!(f, "a lone"),
+            GroupDescriptor::ASingle => write!(f, "a single"),
+            GroupDescriptor::Some => write!(f, "some"),
+            GroupDescriptor::A => write!(f, "a"),
+        }
+    }
+}
