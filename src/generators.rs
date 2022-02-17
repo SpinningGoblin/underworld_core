8fc9c21d9bf943c789e086166a014e7774561f96
Derrick Plotsky <derrickplotsky@gmail.com>
2022-02-16 22:54:08 -0800

    Add name generator
    I will eventually make a name generator per type of species, but right
    now I'm just going to have a single name generator for my characters.

--- src/generators.rs
+++ src/generators.rs
@@ -3,6 +3,7 @@ pub mod fixtures;
 pub mod generator;
 pub mod inventory;
 pub mod items;
+pub mod name;
 pub mod non_players;
 pub mod rooms;
 pub mod stats;
---
+++ src/generators/name.rs
@@ -0,0 +1,31 @@
+use rand::Rng;
+
+const CHANCE_UNNAMED: usize = 10;
+
+pub fn generate_name() -> Option<String> {
+    let mut rng = rand::thread_rng();
+    let no_name_roll: usize = rng.gen_range(1..=100);
+    if no_name_roll <= CHANCE_UNNAMED {
+        return None;
+    }
+
+    let num_parts: usize = rng.gen_range(1..=3);
+
+    let mut all_name_parts = name_parts();
+    let name_parts: Vec<&str> = (0..num_parts)
+        .map(|_| {
+            let index = rng.gen_range(0..all_name_parts.len());
+            all_name_parts.remove(index)
+        })
+        .collect();
+
+    Some(name_parts.join(""))
+}
+
+fn name_parts() -> Vec<&'static str> {
+    vec![
+        "gon", "gro", "grub", "num", "gorg", "zerg", "and", "por", "mer", "mog", "og", "zola",
+        "mar", "ar", "la", "zo", "mank", "mang", "grap", "log", "lorg", "glor", "bor", "bob",
+        "plo", "nor", "norn", "mac", "oor", "onk", "rok", "ron",
+    ]
+}
