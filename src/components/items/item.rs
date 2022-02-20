a8e786dfb6e45e644e92377113b24019186c2102
Derrick Plotsky <derrickplotsky@gmail.com>
2022-02-20 13:52:43 -0800

    Add ItemView
    This adds my first "view" of an item, and builds it out. Views will be
    how I show my components outwards to consumers, probably via commands,
    unless they want to use the components directly. But I think the view
    will be able to help steer things towards "this is the information the
    consumer knows right now".

--- src/components/items/item.rs
+++ src/components/items/item.rs
@@ -30,12 +30,72 @@ pub struct Item {
     pub defense: Option<Defense>,
 }
 
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "bevy_components", derive(Component))]
+#[cfg_attr(
+    feature = "serialization",
+    derive(Deserialize, Serialize),
+    serde(rename_all = "snake_case")
+)]
+pub struct ItemView {
+    pub item_type: ItemType,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub tags: Vec<Tag>,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub descriptors: Vec<Descriptor>,
+    pub descriptors_known: bool,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub material: Option<Material>,
+    pub material_known: bool,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub attack: Option<Attack>,
+    pub attack_known: bool,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub defense: Option<Defense>,
+    pub defense_known: bool,
+}
+
 impl Item {
-    pub fn look_at(&self, is_equipped: bool) -> String {
-        self.describe(is_equipped)
+    pub fn look_at(&self, sees_full_item: bool, knows_all: bool) -> ItemView {
+        let (descriptors, descriptors_known) = if sees_full_item {
+            (self.descriptors.clone(), true)
+        } else {
+            (Vec::new(), false)
+        };
+
+        let (material, material_known) = if sees_full_item {
+            (self.material.clone(), true)
+        } else {
+            (None, false)
+        };
+
+        let (attack, attack_known) = if knows_all {
+            (self.attack.clone(), true)
+        } else {
+            (None, false)
+        };
+
+        let (defense, defense_known) = if knows_all {
+            (self.defense.clone(), true)
+        } else {
+            (None, false)
+        };
+
+        ItemView {
+            item_type: self.item_type.clone(),
+            tags: self.tags.clone(),
+            descriptors,
+            descriptors_known,
+            material,
+            material_known,
+            attack,
+            attack_known,
+            defense,
+            defense_known,
+        }
     }
 
-    fn describe(&self, is_equipped: bool) -> String {
+    pub fn describe(&self, is_equipped: bool) -> String {
         let mut descriptions: Vec<String> = Vec::new();
         self.descriptors
             .iter()
