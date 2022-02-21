54ca9c8d9bc7af6951eb57febc7a9b5175d370ab
Derrick Plotsky <derrickplotsky@gmail.com>
2022-02-20 20:46:47 -0800

    Add views for Character and NonPlayer

--- src/components/character.rs
+++ src/components/character.rs
@@ -3,7 +3,12 @@ use bevy_ecs::prelude::Component;
 #[cfg(feature = "serialization")]
 use serde::{Deserialize, Serialize};
 
-use super::{inventory::Inventory, life_modifier::LifeModifier, species::Species, stats::Stats};
+use super::{
+    inventory::{Inventory, InventoryView},
+    life_modifier::LifeModifier,
+    species::Species,
+    stats::{Stats, StatsView},
+};
 
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "bevy_components", derive(Component))]
@@ -17,7 +22,81 @@ pub struct Character {
     pub inventory: Option<Inventory>,
 }
 
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "bevy_components", derive(Component))]
+#[cfg_attr(feature = "serialization", derive(Deserialize, Serialize))]
+pub struct CharacterView {
+    pub stats: StatsView,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub species: Option<Species>,
+    pub species_known: bool,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub life_modifier: Option<LifeModifier>,
+    pub life_modifier_known: bool,
+    #[cfg_attr(feature = "serialization", serde(default))]
+    pub inventory: Option<InventoryView>,
+    pub inventory_known: bool,
+}
+
+pub struct CharacterViewArgs {
+    pub knows_health: bool,
+    pub knows_species: bool,
+    pub knows_life_modifier: bool,
+    pub knows_inventory: bool,
+    pub knows_hidden_in_inventory: bool,
+    pub knows_packed_in_inventory: bool,
+}
+
 impl Character {
+    pub fn look_at(&self, args: CharacterViewArgs, knows_all: bool) -> CharacterView {
+        let (health, health_known) = if args.knows_health || knows_all {
+            (self.stats.health.clone(), true)
+        } else {
+            (None, false)
+        };
+
+        let (species, species_known) = if args.knows_species || knows_all {
+            (Some(self.species.clone()), true)
+        } else {
+            (None, false)
+        };
+
+        let (life_modifier, life_modifier_known) = if args.knows_life_modifier || knows_all {
+            (self.life_modifier.clone(), true)
+        } else {
+            (None, false)
+        };
+
+        let (inventory, inventory_known) = if args.knows_inventory || knows_all {
+            (
+                self.inventory.clone().map(|inv| {
+                    inv.look_at(
+                        args.knows_hidden_in_inventory,
+                        args.knows_packed_in_inventory,
+                        knows_all,
+                    )
+                }),
+                true,
+            )
+        } else {
+            (None, false)
+        };
+
+        CharacterView {
+            stats: StatsView {
+                health,
+                health_known,
+                height: self.stats.height.clone(),
+            },
+            species,
+            species_known,
+            life_modifier,
+            life_modifier_known,
+            inventory,
+            inventory_known,
+        }
+    }
+
     pub fn describe_species(&self) -> String {
         let mut descriptions: Vec<String> = Vec::new();
 
--- src/components/non_player.rs
+++ src/components/non_player.rs
@@ -3,7 +3,10 @@ use bevy_ecs::prelude::Component;
 #[cfg(feature = "serialization")]
 use serde::{Deserialize, Serialize};
 
-use super::{character::Character, identifier::Identifier};
+use super::{
+    character::{Character, CharacterView, CharacterViewArgs},
+    identifier::{Identifier, IdentifierView},
+};
 
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "bevy_components", derive(Component))]
@@ -13,7 +16,38 @@ pub struct NonPlayer {
     pub identifier: Identifier,
 }
 
+pub struct NonPlayerView {
+    pub character: CharacterView,
+    pub identifier: IdentifierView,
+}
+
 impl NonPlayer {
+    pub fn look_at(
+        &self,
+        character_args: CharacterViewArgs,
+        knows_name: bool,
+        knows_all: bool,
+    ) -> NonPlayerView {
+        let identifier = if knows_name || knows_all {
+            IdentifierView {
+                id: self.identifier.id,
+                name: self.identifier.name.clone(),
+                name_known: true,
+            }
+        } else {
+            IdentifierView {
+                id: self.identifier.id,
+                name: None,
+                name_known: false,
+            }
+        };
+
+        NonPlayerView {
+            identifier,
+            character: self.character.look_at(character_args, knows_all),
+        }
+    }
+
     pub fn describe_name(&self) -> String {
         match &self.identifier.name {
             Some(name) => format!("It says its name is {}", name),
