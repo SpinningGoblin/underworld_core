b55e706475695b4a460eb77121e3fca05b06b872
Derrick Plotsky <derrickplotsky@gmail.com>
2022-02-24 19:38:11 -0800

    Actions are serializable

--- src/actions/action.rs
+++ src/actions/action.rs
@@ -3,6 +3,18 @@ use super::{
     quick_look::QuickLookRoom,
 };
 
+#[cfg(feature = "bevy_components")]
+use bevy_ecs::prelude::Component;
+#[cfg(feature = "serialization")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "bevy_components", derive(Component))]
+#[cfg_attr(
+    feature = "serialization",
+    derive(Deserialize, Serialize),
+    serde(rename_all = "snake_case")
+)]
 pub enum Action {
     LookAtTarget(LookAtTarget),
     LookAtRoom(LookAtRoom),
--- src/actions/quick_look.rs
+++ src/actions/quick_look.rs
@@ -1,3 +1,15 @@
+#[cfg(feature = "bevy_components")]
+use bevy_ecs::prelude::Component;
+#[cfg(feature = "serialization")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "bevy_components", derive(Component))]
+#[cfg_attr(
+    feature = "serialization",
+    derive(Deserialize, Serialize),
+    serde(rename_all = "snake_case")
+)]
 pub struct QuickLookRoom;
 
 impl QuickLookRoom {
