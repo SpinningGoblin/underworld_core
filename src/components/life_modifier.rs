69551f5cd527d1b7df67bb534b7eca167b4aaed3
Derrick Plotsky <derrickplotsky@gmail.com>
2022-02-27 10:56:11 -0800

    Add Dead life modifier

--- src/components/life_modifier.rs
+++ src/components/life_modifier.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 
 use std::fmt::Display;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "bevy_components", derive(Component))]
 #[cfg_attr(
     feature = "serialization",
@@ -16,6 +16,7 @@ use std::fmt::Display;
 )]
 #[cfg_attr(feature = "openapi", derive(Enum), oai(rename_all = "snake_case"))]
 pub enum LifeModifier {
+    Dead,
     Skeleton,
     Vampire,
     Zombie,
@@ -23,20 +24,24 @@ pub enum LifeModifier {
 
 impl Display for LifeModifier {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            Self::Skeleton => write!(f, "skeleton"),
-            Self::Vampire => write!(f, "vampire"),
-            Self::Zombie => write!(f, "zombie"),
-        }
+        let text = match *self {
+            LifeModifier::Skeleton => "skeleton",
+            LifeModifier::Vampire => "vampire",
+            LifeModifier::Zombie => "zombie",
+            LifeModifier::Dead => "dead",
+        };
+
+        write!(f, "{}", text)
     }
 }
 
 impl LifeModifier {
     pub fn as_adjective(&self) -> String {
         match *self {
-            Self::Skeleton => "skeletal".to_string(),
-            Self::Vampire => "vampiric".to_string(),
-            Self::Zombie => "zombified".to_string(),
+            LifeModifier::Skeleton => "skeletal".to_string(),
+            LifeModifier::Vampire => "vampiric".to_string(),
+            LifeModifier::Zombie => "zombified".to_string(),
+            LifeModifier::Dead => "dead".to_string(),
         }
     }
 }
