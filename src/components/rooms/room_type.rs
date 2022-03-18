9b9a2a746f82b9370eae994464346290732829b3
Derrick Plotsky <derrickplotsky@gmail.com>
2022-03-17 19:43:17 -0700

    Derive into enum iterator

--- src/components/rooms/room_type.rs
+++ src/components/rooms/room_type.rs
@@ -2,12 +2,13 @@ use std::fmt::Display;
 
 #[cfg(feature = "bevy_components")]
 use bevy_ecs::prelude::Component;
+use enum_iterator::IntoEnumIterator;
 #[cfg(feature = "openapi")]
 use poem_openapi::Enum;
 #[cfg(feature = "serialization")]
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, IntoEnumIterator)]
 #[cfg_attr(feature = "bevy_components", derive(Component))]
 #[cfg_attr(
     feature = "serialization",
