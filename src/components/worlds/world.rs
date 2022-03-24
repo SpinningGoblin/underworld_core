ca95465dd5f753d5c11b3e4d4eda603d2d94abaa
Derrick Plotsky <derrickplotsky@gmail.com>
2022-03-23 18:26:43 -0700

    Add helper to add room to a world

--- src/components/worlds/world.rs
+++ src/components/worlds/world.rs
@@ -14,6 +14,19 @@ pub struct World {
     pub exit_graph: Vec<ExitMap>,
 }
 
+impl World {
+    pub fn add_room(&mut self, entrance_id: Uuid, room: Room) {
+        if let Some(exit_map) = self
+            .exit_graph
+            .iter_mut()
+            .find(|exit_map| exit_map.exit_id.eq(&entrance_id))
+        {
+            exit_map.set_room_id(room.identifier.id);
+            self.rooms.push(room);
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "bevy_components", derive(Component))]
 #[cfg_attr(feature = "serialization", derive(Deserialize, Serialize))]
@@ -21,4 +34,22 @@ pub struct ExitMap {
     pub exit_id: Uuid,
     pub left_room_id: Option<Uuid>,
     pub right_room_id: Option<Uuid>,
+}
+
+impl ExitMap {
+    pub fn other_room_id(&self, room_id: Uuid) -> Option<Uuid> {
+        if self.left_room_id.eq(&Some(room_id)) {
+            self.right_room_id
+        } else {
+            self.left_room_id
+        }
+    }
+
+    pub fn set_room_id(&mut self, room_id: Uuid) {
+        if self.left_room_id.is_none() {
+            self.left_room_id = Some(room_id);
+        } else if self.right_room_id.is_none() {
+            self.right_room_id = Some(room_id);
+        }
+    }
 }
