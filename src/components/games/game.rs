161408daade98e84cd6e2d3de1b81604e0965960
Derrick Plotsky <derrickplotsky@gmail.com>
2022-03-23 18:25:30 -0700

    Add helper method to get all current exits

--- src/components/games/game.rs
+++ src/components/games/game.rs
@@ -13,6 +13,22 @@ pub struct Game {
     pub identifier: Identifier,
     pub world: World,
     pub player: PlayerCharacter,
-    pub current_room: Uuid,
+    pub current_room_id: Uuid,
     pub rooms_seen: Vec<Uuid>,
+}
+
+impl Game {
+    pub fn current_room_exits(&self) -> Vec<Uuid> {
+        let current_room = self
+            .world
+            .rooms
+            .iter()
+            .find(|room| room.identifier.id.eq(&self.current_room_id))
+            .unwrap();
+        current_room
+            .exits
+            .iter()
+            .map(|exit| exit.identifier.id)
+            .collect()
+    }
 }
