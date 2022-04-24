ca6d0e378b0717adaf125b1a22794778c2692e21
Derrick Plotsky <derrickplotsky@gmail.com>
2022-04-24 14:40:16 -0700

    Add some errors

---
+++ src/errors.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug)]
+#[cfg_attr(
+    feature = "serialization",
+    derive(Deserialize, Serialize),
+    serde(rename_all = "snake_case")
+)]
+pub enum Errors {
+    ExitNotFound(String),
+    ItemNotFound(String),
+    InvalidId(String),
+    NpcNotFound(String),
+}
