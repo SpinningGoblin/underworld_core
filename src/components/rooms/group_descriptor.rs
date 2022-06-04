377065cc9871fc0b16af469104c823e1c4f9b499
Derrick Plotsky <derrickplotsky@gmail.com>
2022-06-03 21:23:04 -0700

    Remove unused GroupDescriptors

--- src/components/rooms/group_descriptor.rs
+++ src/components/rooms/group_descriptor.rs
@@ -17,23 +17,15 @@ use serde::{Deserialize, Serialize};
 #[cfg_attr(feature = "openapi", derive(Enum), oai(rename_all = "snake_case"))]
 pub enum GroupDescriptor {
     A,
-    AFew,
-    AGangOf,
-    AGroupOf,
     ALone,
     ASingle,
-    Some,
 }
 
 impl Display for GroupDescriptor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
-            GroupDescriptor::AFew => write!(f, "a few"),
-            GroupDescriptor::AGangOf => write!(f, "a gang of"),
-            GroupDescriptor::AGroupOf => write!(f, "a group of"),
             GroupDescriptor::ALone => write!(f, "a lone"),
             GroupDescriptor::ASingle => write!(f, "a single"),
-            GroupDescriptor::Some => write!(f, "some"),
             GroupDescriptor::A => write!(f, "a"),
         }
     }
