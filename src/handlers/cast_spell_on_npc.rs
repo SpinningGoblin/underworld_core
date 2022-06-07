9a5a4988c5c57e7c4eab3aea46bbc6528c87d024
Derrick Plotsky <derrickplotsky@gmail.com>
2022-06-06 19:09:07 -0700

    Handle a couple of the spells on NPC

--- src/handlers/cast_spell_on_npc.rs
+++ src/handlers/cast_spell_on_npc.rs
@@ -1,18 +1,20 @@
 use crate::{
     actions::CastSpellOnNpc,
-    components::{games::game_state::GameState, player::PlayerCharacter},
-    errors::{Error, SpellNotFoundError},
-    events::event::Event,
+    components::{
+        games::game_state::GameState, player::PlayerCharacter, spells::spell_name::SpellName,
+    },
+    errors::{Error, NpcNotFoundError, SpellNotFoundError},
+    events::{event::Event, PlayerHitNpc, PlayerSpellForgotten, PlayerSpellUsed},
     utils::ids::parse_id,
 };
 
 pub fn handle(
     cast_spell_on_npc: &CastSpellOnNpc,
-    _state: &GameState,
+    state: &GameState,
     player: &PlayerCharacter,
 ) -> Result<Vec<Event>, Error> {
     let spell_id = parse_id(&cast_spell_on_npc.spell_id)?;
-    let _ = match player.character.find_spell(&spell_id) {
+    let learned_spell = match player.character.find_spell(&spell_id) {
         Some(it) => it,
         None => {
             return Err(Error::SpellNotFoundError(SpellNotFoundError(
@@ -21,5 +23,37 @@ pub fn handle(
         }
     };
 
-    Ok(Vec::new())
+    let npc_id = parse_id(&cast_spell_on_npc.npc_id)?;
+    match &state.current_room().find_npc(&npc_id) {
+        Some(_) => {}
+        None => {
+            return Err(Error::NpcNotFoundError(NpcNotFoundError(
+                npc_id.to_string(),
+            )))
+        }
+    };
+
+    let mut events: Vec<Event> = Vec::new();
+    match learned_spell.spell.name {
+        SpellName::ElectricBlast | SpellName::RagingFireball => {
+            let damage = learned_spell.spell.damage();
+            events.push(Event::PlayerHitNpc(PlayerHitNpc {
+                attacker_id: player.id,
+                npc_id,
+                damage,
+            }));
+        }
+        // TODO: There are non-damage spells that someone could cast on NPCs.
+        _ => {}
+    }
+
+    events.push(Event::PlayerSpellUsed(PlayerSpellUsed { spell_id }));
+
+    if learned_spell.spell.uses - 1 == 0 {
+        events.push(Event::PlayerSpellForgotten(PlayerSpellForgotten {
+            spell_id,
+        }));
+    }
+
+    Ok(events)
 }
