95e5e6716545eee961f69382bd1ff17a1586a3ae
Derrick Plotsky <derrickplotsky@gmail.com>
2022-06-27 22:31:48 -0700

    Health is higher at high danger levels
    More health. Way more health.

--- src/generators/stats.rs
+++ src/generators/stats.rs
@@ -15,11 +15,27 @@ pub fn build_specific_health(max_health: i32) -> StatsPrototype {
     }
 }
 
+pub fn build_specific_health_danger_level(max_health: i32, danger_level: u32) -> StatsPrototype {
+    StatsPrototype {
+        max_health: Some(max_health),
+        num_health_rolls: 0,
+        danger_level,
+    }
+}
+
 pub fn build(num_health_rolls: usize) -> StatsPrototype {
     StatsPrototype {
         max_health: None,
         num_health_rolls,
         danger_level: 1,
+    }
+}
+
+pub fn build_danger_level(num_health_rolls: usize, danger_level: u32) -> StatsPrototype {
+    StatsPrototype {
+        max_health: None,
+        num_health_rolls,
+        danger_level,
     }
 }
 
@@ -91,24 +107,38 @@ impl Generator<Stats> for StatsPrototype {
         let max_health = match self.max_health {
             Some(max) => max,
             _ => {
-                let additional_rolls = if (1..=10).contains(&self.danger_level) {
+                let additional_rolls = if (1..=5).contains(&self.danger_level) {
                     0
-                } else if (11..=20).contains(&self.danger_level) {
-                    1
+                } else if (6..=10).contains(&self.danger_level) {
+                    2
+                } else if (11..=15).contains(&self.danger_level) {
+                    4
+                } else if (16..=20).contains(&self.danger_level) {
+                    5
                 } else if (21..=30).contains(&self.danger_level) {
-                    3
-                } else {
                     6
+                } else {
+                    8
                 };
 
                 let num_rolls = self.num_health_rolls + additional_rolls;
                 let range = 0..num_rolls;
 
+                let min_health_roll = if (1..=10).contains(&self.danger_level) {
+                    1
+                } else if (11..=30).contains(&self.danger_level) {
+                    2
+                } else {
+                    3
+                };
+
                 if range.is_empty() {
                     0
                 } else {
                     let mut rng = rand::thread_rng();
-                    range.map(|_| roll_d6(&mut rng, 1, 0)).sum()
+                    range
+                        .map(|_| min_health_roll.max(roll_d6(&mut rng, 1, 0)))
+                        .sum()
                 }
             }
         };
