38be675310a1d1b49c31fd20936d42b076bd09de
Derrick Plotsky <derrickplotsky@gmail.com>
2022-08-29 20:22:42 -0700

    Move builder to own module

--- src/generators/rooms.rs
+++ src/generators/rooms.rs
@@ -1,7 +1,10 @@
+mod builder;
 mod dimensions;
 mod exits;
 mod fixtures;
 pub mod npcs;
+
+pub use builder::RoomGeneratorBuilder;
 
 use std::ops::RangeInclusive;
 
@@ -28,118 +31,6 @@ struct RoomPrototype {
     pub include_flavour_text: bool,
     pub name: Option<String>,
     pub dimensions: Option<Dimensions>,
-}
-
-#[derive(Default)]
-pub struct RoomGeneratorBuilder {
-    num_descriptors: Option<RangeInclusive<usize>>,
-    room_type: Option<RoomType>,
-    possible_descriptors: Option<Vec<Descriptor>>,
-    entrance_id: Option<Uuid>,
-    danger_level: Option<u32>,
-    include_flavour_text: Option<bool>,
-    possible_flavour_texts: Option<Vec<Flavour>>,
-    name: Option<String>,
-    dimensions: Option<Dimensions>,
-}
-
-impl RoomGeneratorBuilder {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn num_descriptors(&mut self, num_descriptors: RangeInclusive<usize>) -> &mut Self {
-        self.num_descriptors = Some(num_descriptors);
-
-        self
-    }
-
-    pub fn room_type(&mut self, room_type: RoomType) -> &mut Self {
-        self.room_type = Some(room_type);
-
-        self
-    }
-
-    pub fn possible_descriptors(&mut self, possible_descriptors: Vec<Descriptor>) -> &mut Self {
-        self.possible_descriptors = Some(possible_descriptors);
-
-        self
-    }
-
-    pub fn entrance_id(&mut self, entrance_id: Uuid) -> &mut Self {
-        self.entrance_id = Some(entrance_id);
-
-        self
-    }
-
-    pub fn danger_level(&mut self, danger_level: u32) -> &mut Self {
-        self.danger_level = Some(danger_level);
-
-        self
-    }
-
-    pub fn include_flavour_text(&mut self, include_flavour: bool) -> &mut Self {
-        self.include_flavour_text = Some(include_flavour);
-
-        self
-    }
-
-    pub fn possible_flavour_texts(&mut self, possible_flavour_texts: Vec<Flavour>) -> &mut Self {
-        self.possible_flavour_texts = Some(possible_flavour_texts);
-
-        self
-    }
-
-    pub fn name(&mut self, name: &str) -> &mut Self {
-        self.name = Some(name.to_string());
-
-        self
-    }
-
-    pub fn dimensions(&mut self, dimensions: Dimensions) -> &mut Self {
-        self.dimensions = Some(dimensions);
-
-        self
-    }
-
-    pub fn build(&self) -> impl Generator<Room> {
-        let num_descriptors = match &self.num_descriptors {
-            Some(it) => it.clone(),
-            None => 0..=2,
-        };
-
-        let room_type = match &self.room_type {
-            Some(it) => it.clone(),
-            None => {
-                let room_types: Vec<RoomType> = RoomType::iter().collect();
-                let mut rng = rand::thread_rng();
-                let index = rng.gen_range(0..room_types.len());
-                room_types.get(index).unwrap().clone()
-            }
-        };
-
-        let possible_descriptors = match &self.possible_descriptors {
-            Some(it) => it.clone(),
-            None => room_type.possible_descriptors(),
-        };
-
-        let possible_flavour_texts = match &self.possible_flavour_texts {
-            Some(it) => it.clone(),
-            None => room_type.possible_flavours(),
-        };
-
-        RoomPrototype {
-            num_descriptors,
-            room_type,
-            possible_descriptors,
-            entrance_id: self.entrance_id,
-            danger_level: self.danger_level.unwrap_or(1),
-            possible_flavour_texts,
-            include_flavour_text: self.include_flavour_text.unwrap_or(true),
-            name: self.name.clone(),
-            dimensions: self.dimensions.clone(),
-        }
-    }
 }
 
 impl Generator<Room> for RoomPrototype {
