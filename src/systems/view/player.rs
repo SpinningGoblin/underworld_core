0b7bbc78370db143f47f2654367385db7254d07a
Derrick Plotsky <derrickplotsky@gmail.com>
2022-09-12 22:58:13 -0700

    Use reference instead

--- src/systems/view/player.rs
+++ src/systems/view/player.rs
@@ -2,7 +2,7 @@ use crate::components::{
     CharacterViewArgs, {PlayerCharacter, PlayerCharacterView},
 };
 
-pub fn check(player_character: PlayerCharacter) -> PlayerCharacterView {
+pub fn check(player_character: &PlayerCharacter) -> PlayerCharacterView {
     let args = CharacterViewArgs {
         knows_health: true,
         knows_inventory: true,
@@ -14,6 +14,6 @@ pub fn check(player_character: PlayerCharacter) -> PlayerCharacterView {
         character,
         id: player_character.id.to_string(),
         gold: player_character.gold,
-        name: player_character.name,
+        name: player_character.name.clone(),
     }
 }
